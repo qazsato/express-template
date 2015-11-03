17fd6e36d87957f2579ef5373b39cc486fc8724c
qaz.s <qaz.s.co@gmail.com>
2015-11-03 16:54:28 +0900

    styleタスクをcssとsassに分離

--- gulpfile.js
+++ gulpfile.js
@@ -17,8 +17,9 @@ var AUTOPREFIXER_BROWSERS = [
 
 var SRC_PATH = {
   ROOT   : './public/src',
-  SCRIPT : ['./public/src/**/*.js'],
-  STYLE  : ['./public/src/**/*.+(css|scss)'],
+  JS     : ['./public/src/**/*.js'],
+  CSS    : ['./public/src/**/*.css'],
+  SASS   : ['./public/src/**/*.scss'],
   IMAGE  : ['./public/src/**/*.+(jpg|jpeg|png|gif|svg)'],
   OTHER  : ['./public/src/**/*.*', '!./public/src/**/*.+(js|css|scss|jpg|jpeg|png|gif|svg)']
 };
@@ -27,21 +28,28 @@ var DEST_PATH = {
   ROOT   : './public/dist'
 };
 
-gulp.task('script', function () {
-  gulp.src(SRC_PATH.SCRIPT)
+gulp.task('js', function () {
+  gulp.src(SRC_PATH.JS)
       .pipe($.plumber({errorHandler: $.notify.onError('Error: <%= error.message %>')}))
       .pipe($.if(ENV === 'production', $.uglify()))
       .pipe(gulp.dest(DEST_PATH.ROOT));
 });
 
-gulp.task('style', function () {
-  gulp.src(SRC_PATH.STYLE)
+gulp.task('css', function () {
+  gulp.src(SRC_PATH.CSS)
       .pipe($.plumber({errorHandler: $.notify.onError('Error: <%= error.message %>')}))
-      .pipe($.if('*.css', $.csso()))
-      .pipe($.if('*.css', $.cssmin()))
-      .pipe($.if('*.scss' && ENV !== 'production', $.sass()))
-      .pipe($.if('*.scss' && ENV === 'production', $.sass({outputStyle: 'compressed'})))
-      .pipe($.if('*.scss', $.cached('sass')))
+      .pipe($.csso())
+      .pipe($.cssmin())
+      .pipe($.autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
+      .pipe(gulp.dest(DEST_PATH.ROOT));
+});
+
+gulp.task('sass', function () {
+  gulp.src(SRC_PATH.SASS)
+      .pipe($.plumber({errorHandler: $.notify.onError('Error: <%= error.message %>')}))
+      .pipe($.sass())
+      .pipe($.cached('sass'))
+      .pipe($.if(ENV === 'production', $.cssmin()))
       .pipe($.autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
       .pipe(gulp.dest(DEST_PATH.ROOT));
 });
@@ -64,8 +72,9 @@ gulp.task('clean', function () {
 });
 
 gulp.task('watch', function () {
-  gulp.watch(SRC_PATH.SCRIPT, ['script']);
-  gulp.watch(SRC_PATH.STYLE, ['style']);
+  gulp.watch(SRC_PATH.JS, ['js']);
+  gulp.watch(SRC_PATH.CSS, ['css']);
+  gulp.watch(SRC_PATH.SASS, ['sass']);
   gulp.watch(SRC_PATH.IMAGE, ['image']);
   gulp.watch(SRC_PATH.OTHER, ['other']);
 });
@@ -84,5 +93,5 @@ gulp.task('development', function () {
 });
 
 gulp.task('production', function () {
-  runSequence('clean', ['script', 'style', 'image', 'other']);
+  runSequence('clean', ['js', 'css', 'sass', 'image', 'other']);
 });
