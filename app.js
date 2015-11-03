aa966c257669ee72c7e313f661e11f45ad0642fc
qaz.s <qaz.s.co@gmail.com>
2015-11-04 01:15:13 +0900

    エラーハンドリング処理統合

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// require node_modules
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -6,6 +7,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var config = require('config');
 
+// require routes
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -33,26 +35,11 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
-    error: {}
+    error: app.get('env') === 'development' ? err : {}
   });
 });
 
