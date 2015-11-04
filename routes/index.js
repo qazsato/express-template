f6b1c0d8d590438b5b2d275a4a6df826458856fa
qaz.s <qaz.s.co@gmail.com>
2015-11-04 22:15:39 +0900

    同期処理用モジュールasync追加

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,38 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var async = require('async');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var url = 'http://urls.api.twitter.com/1/urls/count.json?url=https://www.google.com/';
-  request(url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log(JSON.parse(body));
-    }
-    res.render('index', { title: 'Express' });
-  });
+
+  async.series(
+    [
+      function (callback) {
+        // twitter API
+        var url = 'http://urls.api.twitter.com/1/urls/count.json?url=https://www.google.com/';
+        request(url, function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            console.log(JSON.parse(body));
+          }
+          callback();
+        });
+      },
+      function (callback) {
+        // Facebook API
+        var url = 'http://graph.facebook.com/?id=https://www.google.com/';
+        request(url, function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            console.log(JSON.parse(body));
+          }
+          callback();
+        });
+      }
+    ],
+    function (err, results) {
+      if (err) throw err;
+      res.render('index', { title: 'Express' });
+    });
 });
 
 module.exports = router;
