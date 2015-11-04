fc938a482fe17a05def66061e1c9e2b38a34b9e4
qaz.s <qaz.s.co@gmail.com>
2015-11-04 22:01:14 +0900

    通信用モジュールのrequest追加

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var url = 'http://urls.api.twitter.com/1/urls/count.json?url=https://www.google.com/';
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(JSON.parse(body));
+    }
+    res.render('index', { title: 'Express' });
+  });
 });
 
 module.exports = router;
