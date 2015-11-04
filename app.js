1a4cdfb7cc6ed5ab85a3f3a6a5ec3abd0fac349a
qaz.s <qaz.s.co@gmail.com>
2015-11-04 21:27:00 +0900

    gzip転送をおこなうためcompressionを追加

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var config = require('config');
+var compression = require('compression');
 
 // require routes
 var routes = require('./routes/index');
@@ -22,6 +23,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
