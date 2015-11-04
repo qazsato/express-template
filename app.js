95631b431c1be97e031dcd62adc6eb36b3073205
qaz.s <qaz.s.co@gmail.com>
2015-11-04 21:50:27 +0900

    app.js整理

--- app.js
+++ app.js
@@ -1,38 +1,39 @@
 // require node_modules
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('serve-favicon');
+var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var config = require('config');
-var compression = require('compression');
+var bodyParser   = require('body-parser');
+var config       = require('config');
+var compression  = require('compression');
 
+// create app
 var app = express();
 
-// view engine setup
+// set app
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// mount app
 app.use(favicon(path.join(__dirname, 'public/dist/images/favicon/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(compression());
 app.use(express.static(path.join(__dirname, 'public'), {maxAge: 86400000 * 30}));
 
+// define routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 
-// catch 404 and forward to error handler
+// error handlers
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
-
-// error handlers
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
@@ -40,6 +41,5 @@ app.use(function(err, req, res, next) {
     error: app.get('env') === 'development' ? err : {}
   });
 });
-
 
 module.exports = app;
