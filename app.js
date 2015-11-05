e6387fac4fa88371b9e38c542eced16b16ddd9dd
qaz.s <qaz.s.co@gmail.com>
2015-11-05 23:27:46 +0900

    ロゴ画像追加

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // mount app
-app.use(favicon(path.join(__dirname, 'public/dist/images/favicon/favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public/dist/images/common/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
