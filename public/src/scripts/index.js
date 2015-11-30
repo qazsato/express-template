da52735b232a88454d098c770df1115558b2f0ed
qaz.s <qaz.s.co@gmail.com>
2015-11-30 22:25:37 +0900

    vendorフォルダ削除(scripts配下)

--- public/src/scripts/index.js
+++ public/src/scripts/index.js
@@ -1,6 +1,6 @@
 // import
-window.jQuery = $ = require('./vendor/jquery-2.1.4.min');
-require('./vendor/bootstrap.min');
+window.jQuery = $ = require('../components/jquery/jquery-2.1.4.min');
+require('../components/bootstrap/js/bootstrap');
 
 $(function () {
   console.log("ready");
